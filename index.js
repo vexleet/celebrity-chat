242289d01633986381613d8a428cf459cfe66dbb
vexleet <vex_malamov@abv.bg>
2018-07-28 21:39:42 +0300

    Users can chat with each other now

---
+++ index.js
@@ -0,0 +1,22 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('disconnect', function (socket) {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on http://localhost:3000');
+});
