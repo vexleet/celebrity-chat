455a1c690966fac23f85a57962ead8b12a9e7839
vexleet <vex_malamov@abv.bg>
2018-07-28 23:47:24 +0300

    Added usernames

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let users = {};
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.broadcast.emit('hi');
-    socket.on('disconnect', function (socket) {
-        console.log('user disconnected');
+io.on('connection', function (socket) {
+    socket.broadcast.emit('connection');
+
+    socket.on('disconnect', function () {
+        io.emit('disconnect');
     });
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+    socket.on('chat message', function (msg, nickname) {
+        socket.broadcast.emit('chat message', { nickname: nickname, text: msg });
+    });
+    socket.on('nickname', function (name) {
+        if(users[name] == undefined){
+            let socketId = socket.id;
+            var userId = Object.keys(users).length + 100;
+            users[socketId] = {
+                id: userId,
+                nickname: name,
+            }
+        }
+        io.emit('nickname', users);
+    });
+    socket.on('is typing', function () {
+        io.emit('is typing');
+    });
+    socket.on('is not typing', function () {
+        io.emit('is not typing');
     });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on http://localhost:3000');
 });
