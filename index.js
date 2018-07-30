253241c7e56f5692822418267cca5d5def2dfb2c
vexleet <vex_malamov@abv.bg>
2018-07-30 23:03:03 +0300

    Added more celeb names and fixed some bugs

--- index.js
+++ index.js
@@ -2,69 +2,8 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-let celebrities = [
-    'Floyd Mayweather',
-    'George Clooney',
-    'Kylie Jenner',
-    'Judy Sheindlin',
-    'Dwayne Johnson',
-    'Coldplay',
-    'Lionel Messi',
-    'Ed Sheeran',
-    'Cristiano Ronaldo',
-    'Bruno Mars',
-    'Conor McGregor',
-    'Neymar',
-    'Howard Stern',
-    'Ellen DeGeneres',
-    'LeBron James',
-    'Rush Limbaugh',
-    'Katy Perry',
-    'Robert Downey Jr.',
-    'Taylor Swift',
-    'Dr. Phil McGraw',
-    'Roger Federer',
-    'Stephen Curry',
-    'Jay-Z',
-    'Ryan Seacrest',
-    'Roger Waters',
-    'Matt Ryan',
-    'Kim Kardashian West',
-    'Chris Hemsworth',
-    'Sean Combs',
-    'David Copperfield',
-    'Gordon Ramsay',
-    'Beyoncé Knowles',
-    'Matthew Stafford',
-    'Kendrick Lamar',
-    'Jerry Seinfeld',
-    'Kevin Hart',
-    'The Weeknd',
-    'J.K. Rowling',
-    'Kevin Durant',
-    'Luke Bryan',
-    'Pink',
-    'Jimmy Buffett',
-    'Lewis Hamilton',
-    'Lady Gaga',
-    'Calvin Harris',
-    'Paul McCartney',
-    'Russell Westbrook',
-    'Drake',
-    'Jennifer Lopez',
-    'James Harden',
-    'Elton John',
-    'Jackie Chan',
-    'Steve Harvey',
-    'Tiger Woods',
-    'Drew Brees',
-    'Sofía Vergara',
-    'Will Smith',
-    'Rafael Nadal',
-    'Rihanna',
-    'Kenny Chesney'
-];
-
+let fs = require("fs");
+let celebrities = JSON.parse(fs.readFileSync('celebrities.json', 'utf8'));
 let users = {};
 
 app.get('/', function (req, res) {
@@ -75,7 +14,7 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     let randCeleb = celebrities[Math.floor(Math.random() * celebrities.length)];
-    if(users[randCeleb] === undefined){
+    if (users[randCeleb] === undefined) {
         let socketId = socket.id;
         let userId = Object.keys(users).length + 100;
         users[socketId] = {
@@ -83,29 +22,27 @@ io.on('connection', function (socket) {
             nickname: randCeleb,
         };
     }
-    console.log(randCeleb);
 
     socket.broadcast.emit('connection', randCeleb, users);
     socket.emit('connection', 'false', users);
 
     socket.on('disconnect', function () {
-        if(users[socket.id]) {
+        if (users[socket.id]) {
             let username = users[socket.id].nickname;
             delete users[socket.id];
-            io.emit('disconnect',username, users);
+            io.emit('disconnect', username, users);
         }
-        else{
+        else {
             io.emit('disconnect');
         }
     });
     socket.on('chat message', function (msg, nickname) {
-        io.emit('chat message', { nickname: nickname, text: msg });
+        io.emit('chat message', {nickname: nickname, text: msg});
     });
     socket.on('is typing', function (nickname, className) {
         socket.broadcast.emit('is typing', nickname, className);
     });
     socket.on('is not typing', function (nickname) {
-        console.log(nickname);
         io.emit('is not typing', nickname);
     });
 });
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,4 @@
-$(function() {
-    console.log(false);
+$(function () {
     let socket = io();
     let isWriting = false;
     let allUsers = {};
@@ -7,61 +6,71 @@ $(function() {
     $('.chat').submit(function () {
         let nickname = '';
         nickname = allUsers[socket.id].nickname;
-        if($('#m').val().length >= 1) {
+
+        if ($('#m').val().length >= 1) {
             socket.emit('chat message', $('#m').val(), nickname);
         }
+
         $('#m').val('');
         return false;
     });
 
     $('#m').keyup(function () {
+
         if ($('#m').val().length >= 1 && isWriting === false) {
             let nickname = '';
             nickname = allUsers[socket.id].nickname.split(' ');
             isWriting = true;
-            console.log(isWriting);
             socket.emit('is typing', nickname, nickname[nickname.length - 1].toLowerCase());
         }
+
         if ($('#m').val().length === 0 && isWriting) {
             let nickname = '';
             nickname = allUsers[socket.id].nickname.split(' ');
-            console.log(nickname);
             isWriting = false;
             socket.emit('is not typing', nickname[nickname.length - 1].toLowerCase());
         }
     });
+
     socket.on('connection', function (nickname, users) {
         allUsers = users;
-        if(nickname !== 'false') {
+
+        if (nickname !== 'false') {
             $('#messages').append($('<li>').text(`${nickname} connected`));
         }
+
         showOnlineUsers();
     });
+
     socket.on('disconnect', function (nickname, users) {
-        if(nickname !== 'transport close') {
-            $('#messages').append($('<li>').text(`${nickname} disconnected`));
+
+        if (nickname !== 'transport close') {
+            nickname = nickname.split(' ');
+            $(`.${nickname[nickname.length - 1].toLowerCase()}`).remove();
+            $('#messages').append($('<li>').text(`${nickname.join(' ')} disconnected`));
             allUsers = users;
-            console.log(allUsers);
             showOnlineUsers();
         }
+
     });
+
     socket.on('chat message', function (msg) {
         $('#messages').append($('<li>').text(`${msg.nickname}: ${msg.text}`));
     });
+
     socket.on('is typing', function (nickname, className) {
         $('#messages').append($('<li>').text(`${nickname.join(' ')} is typing`).addClass(className));
     });
+
     socket.on('is not typing', function (nickname) {
         $(`.${nickname}`).remove();
     });
 
-    function showOnlineUsers(){
-        console.log(true);
+    function showOnlineUsers() {
         $('#users').empty();
-        $('#users').append($('<li>').text('Online Users'))
-        for(let key in allUsers){
+        $('#users').append($('<li>').text('Online Users'));
+        for (let key in allUsers) {
             $('#users').append($('<li>').text(allUsers[key].nickname))
         }
     }
-
 });
